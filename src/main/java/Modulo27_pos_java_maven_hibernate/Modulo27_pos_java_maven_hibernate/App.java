3277aaefdd93ff074e3c18bfa563a4229f23e6cc
Daniel Jara <daniel11jara@gmail.com>
2024-06-27 22:38:04 -0300

    inicio do projeto

---
+++ src/main/java/Modulo27_pos_java_maven_hibernate/Modulo27_pos_java_maven_hibernate/App.java
@@ -0,0 +1,13 @@
+package Modulo27_pos_java_maven_hibernate.Modulo27_pos_java_maven_hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Modulo27_pos_java_maven_hibernate/Modulo27_pos_java_maven_hibernate/AppTest.java
@@ -0,0 +1,20 @@
+package Modulo27_pos_java_maven_hibernate.Modulo27_pos_java_maven_hibernate;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
